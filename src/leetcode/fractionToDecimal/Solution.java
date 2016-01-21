7ef3b0bc118c3f1383874681841d5e04799f6f5a
webai <webai@paypal.com>
2016-01-21 11:08:05 +0800

    create sklenton

---
+++ src/leetcode/fractionToDecimal/Solution.java
@@ -0,0 +1,14 @@
+package leetcode.fractionToDecimal;
+
+public class Solution {
+
+	public String fractionToDecimal(int numerator, int denominator) {
+		return null;
+	        
+	    }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
